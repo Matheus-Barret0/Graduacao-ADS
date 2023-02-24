492d4797a54d7469d10e00dd287dc8156e935370
Matheus_Barreto <matheusbarreto.dev@gmail.com>
2023-02-24 14:23:05 -0300

    Atividade

---
+++ Linguagem_de_Programação_02_Python/indentificarLetras.py
@@ -0,0 +1,8 @@
+#identificar se o usuario digitou uma vogal ou uma consoante
+
+letra = input('Digite uma letra: ')
+if letra == ('a' or 'e' or 'i' or 'o' or 'u'):
+    print('Vogal')
+else:
+    print('Consuante')
+
